99b289892d01bb64cccb45d4993fcb03d5ca526f
Anthony <113693735+AnthonyCostaCrosciatte@users.noreply.github.com>
2024-11-09 10:52:59 -0300

    Add files via upload

--- projetobdviews/funcoes/usuarios.php
+++ projetobdviews/funcoes/usuarios.php
@@ -13,7 +13,6 @@ function login(string $email, string $senha){
     $usuario = $stament->fetchAll(PDO::FETCH_ASSOC);
     //verifica se o usuário não existe, se não existir, vamos criar
     if (!$usuario){
-        $senha = password_hash('adm', PASSWORD_BCRYPT);
         novoUsuario('Administrador','adm@adm.com', $senha, 'adm');
     }
 
